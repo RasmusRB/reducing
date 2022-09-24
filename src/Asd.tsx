7736c2860093484f878a7822bbce54def6977af4
Rasmusrb <rasmusbergstroem87@gmail.com>
2022-09-24 12:46:37 +0200

    back btn

--- src/Asd.tsx
+++ src/Asd.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 
 const Asd = () => {
 
     const { count } = useSelector((state: any) => state.counter)
+    const navigate = useNavigate() 
+
+   
 
     return (
         <div>
             {count}
+            <button onClick={() => navigate("/")}>Back</button>
         </div>
     )
 }
--- src/Counting.tsx
+++ src/Counting.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Counter from "./Counter";
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment, incrementByAmount } from "./redux/counter";
 import { Link } from "react-router-dom";
@@ -15,7 +14,6 @@ const Counting = () => {
             <button onClick={() => dispatch(increment())}>increment</button>
             <button onClick={() => dispatch(decrement())}>decrement</button>
             <button onClick={() => dispatch(incrementByAmount(33))}>increment by 33</button>
-            <Counter />
             <Link to="/asd">asdasd</Link>
         </div>
     )
